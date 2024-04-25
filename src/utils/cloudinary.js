0c8eb281c084717a375a0ad3ad25884c6edfcf9f
peachjelly13 <ananya.sharma2021a@vitstudent.ac.in>
2024-04-25 18:43:57 +0530

    cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -6,3 +6,20 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try{
+        if(!localFilePath) return null   //you do not know what do you want to do
+        //now we are uploading the file on cloudinary
+        cloudinary.uploader.upload() //give the name of your local file here
+ 
+    }
+    catch(error){
+
+    }
+
+}
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
